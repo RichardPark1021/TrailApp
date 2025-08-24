06f6666c4dcad4d1cf53ece123553339b0c81177
Richard Park <122904139+RichardPark1021@users.noreply.github.com>
2025-08-24 09:45:24 -0400

    Update index.js

--- TrailApp/client/src/api/index.js
+++ TrailApp/client/src/api/index.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://trailapp-backend.onrender.com' });
 
 
 export const signUp = (formData) => API.post('/api/user/signup', formData);
